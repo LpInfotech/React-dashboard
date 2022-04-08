86aa6335ce44fb31725d09d28d0bf0773d8e5b22
Sunil <skumar@constellant.com>
2022-04-08 19:03:52 +0530

    dashboard

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,14 +13,9 @@ export default class Card extends Component {
 <div className={`card ${this.props && this.props.headerProps}`}>
   {/* <img src="..." className="card-img-top" alt="..."/> */}
   <div className="card-body">
-   <h5 className="card-title mb-0">{this.props.title}</h5>
-   {this.props.bodyTitle && <h1 className="mb-0">{this.props.bodyIcon && <span>{this.props.bodyIcon}</span>}{this.props.bodyTitle}{this.props.month && <small className="fs-6">{this.props.month}</small>}</h1>}
+   <h5 className="card-title">{this.props.title}</h5>
+   {this.props.bodyTitle && <h1>{this.props.bodyIcon && <span>{this.props.bodyIcon}</span>}{this.props.bodyTitle}{this.props.month && <small className="fs-6">{this.props.month}</small>}</h1>}
     <p className="card-text">{this.props.text}</p>
-    {this.props.list && <ul className="list-unstyled">
-        {this.props.list.split(",").map((item, index)=>{
-        return <li key={index}>{item}</li>
-        })}
-    </ul>}
     <Link to="/" className="btn btn-primary">Go somewhere</Link>
   </div>
 </div>
--- src/pages/Offer/Offer.js
+++ src/pages/Offer/Offer.js
@@ -7,9 +7,10 @@ export class Offer extends Component {
       <section id="offer">
       <div className="container py-5">
         <div className="row">
-          <div className="col-4"><Card headerProps={"bg-light border-0 shadow-sm"} bodyIcon={'$'} bodyTitle={'18 /'} month={'month'} list={'test,test'}  title={'Free'} icon={"fa-solid fa-lock-open"} text={"Lorem ipsum dolor"}/></div>
-          <div className="col-4"><Card headerProps={"bg-light border-0 shadow-sm"} list={'test,test'} month={'month'} bodyTitle={'18 /'} bodyIcon={'$'} title={'Pro'} icon={"fas fa-lock fa-fw"} text={"Lorem ipsum dolor"}/></div>
-          <div className="col-4"><Card headerProps={"bg-light border-0 shadow-sm"} list={'test,test'} month={'month'} bodyTitle={'18 /'} bodyIcon={'$'} title={'Enterprise'} icon={"fas fa-lock fa-fw"} text={"Lorem ipsum dolor"}/></div>
+        <div className="col-3"><Card headerProps={"border-0 shadow"} title={'Free'} bodyTitle={'0 /'} bodyIcon={'$'} month={'month'} icon={"fa-solid fa-lock-open"} text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}/></div>
+          <div className="col-3"><Card headerProps={"border-0 shadow"} bodyIcon={'$'} bodyTitle={'18 /'} month={'month'}   title={'Basic'} icon={"fa-solid fa-lock-open"} text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}/></div>
+          <div className="col-3"><Card headerProps={"border-0 shadow"} month={'month'} bodyTitle={'18 /'} bodyIcon={'$'} title={'Pro'} icon={"fas fa-lock fa-fw"} text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}/></div>
+          <div className="col-3"><Card headerProps={"border-0 shadow"} month={'month'} bodyTitle={'18 /'} bodyIcon={'$'} title={'Enterprise'} icon={"fas fa-lock fa-fw"} text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}/></div>
         </div>
       </div>
       </section>
